b8fd63280fd3e09e8f9268b550b0933f2d76525d
RamejaAyush <ayushrwork@gmail.com>
2022-03-24 09:21:14 +0530

    Nav changes and Login added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,15 @@
+import LoginImg from "../Assert/Images/Login.png";
+import "../Styles/Login.scss";
+
 const Login = () => {
- return (
-  <div className="Signup form">
-   <h1>Login</h1>
-  </div>
- );
+  return (
+    <div className="login">
+      <div className="img-container">
+        <img src={LoginImg} alt="" />
+      </div>
+      <div className="form-container"></div>
+    </div>
+  );
 };
 
 export default Login;
