ac4c7c547cc82057e61ae7194f60c0530aeb5e71
RamejaAyush <ayushrwork@gmail.com>
2022-03-24 10:29:55 +0530

    Login Page Designed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,55 @@
-import LoginImg from "../Assert/Images/Login.png";
+import { Link } from "react-router-dom";
+import { useRef, useEffect } from "react";
 import "../Styles/Login.scss";
+import LoginImg from "../Assert/Images/Login.png";
 
 const Login = () => {
+  let input = useRef(null);
+
+  useEffect(() => {
+    if (input) {
+      console.log(input.innerText);
+    } else {
+      console.log("empty");
+    }
+  }, []);
+
   return (
     <div className="login">
       <div className="img-container">
         <img src={LoginImg} alt="" />
       </div>
-      <div className="form-container"></div>
+      <div className="form-container">
+        <div className="login-form">
+          <form action="#">
+            <header>
+              <h1>Login</h1>
+            </header>
+            <div className="inputs">
+              <div className="email input">
+                <input type="text" required />
+                <div className="placeholder">
+                  <p>Email</p>
+                </div>
+              </div>
+              <div className="Password input">
+                <input type="Password" required />
+                <div className="placeholder">
+                  <p>Password</p>
+                </div>
+              </div>
+            </div>
+            <div className="buttons">
+              <button className="submit">Let's Go</button>
+              <a href="#">Having Issues?</a>
+            </div>
+          </form>
+        </div>
+        <div className="signUp">
+          <h2>New User?</h2>
+          <Link to="/signup">SignUp</Link>
+        </div>
+      </div>
     </div>
   );
 };
